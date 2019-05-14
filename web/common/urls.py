ca77b850d48efd22fc63227744adc0db6a784251
rddimon <rddimon@gmail.com>
2019-05-14 15:48:06 +0300

    improvement web app

--- web/common/urls.py
+++ web/common/urls.py
@@ -5,7 +5,12 @@ from flask import Blueprint
 
 from . import views
 
-COMMON_BLUEPRINT = Blueprint('common', __name__, static_folder='static', template_folder='templates')
+COMMON_BLUEPRINT = Blueprint(
+    'common', __name__,
+    static_folder='static',
+    static_url_path='/static/common',
+    template_folder='templates'
+)
 
 COMMON_BLUEPRINT.add_url_rule(
     '/', view_func=views.RenderTemplateView.as_view('index', template_name='base.html')
